3a55d6820186c1b99487f88bcaac846221e3f14e
Jan "aso" Szenborn <aso.gdynia@gmail.com>
2015-11-10 15:42:40 +0100

    Credits box

---
+++ scripts/ui.js
@@ -0,0 +1,15 @@
+function showCredits() {
+   // Show gray overlay
+   $('#gray_overlay').css('display', 'block');
+
+   // Show credits box
+   $('#credits_overlay').css('display', 'block');
+}
+
+function closeCredits() {
+   // Hide gray overlay
+   $('#gray_overlay').css('display', 'none');
+
+   // Hide credits box
+   $('#credits_overlay').css('display', 'none');
+}
