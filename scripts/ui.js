8b59dc94f29adecce7455931d2a41cdfd2eb58a2
Jan "aso" Szenborn <aso.gdynia@gmail.com>
2015-11-10 17:12:28 +0100

    Resizing canvas to keep aspect ratio

--- scripts/ui.js
+++ scripts/ui.js
@@ -13,3 +13,20 @@ function closeCredits() {
    // Hide credits box
    $('#credits_overlay').css('display', 'none');
 }
+
+function resizeCanvas() {
+   // Set canvas dimensions to keep aspect ratio
+   $('#mainCanvas').css('width', $('main').height() / 5 * 4);
+   $('#mainCanvas').css('height', $('main').height());
+}
+
+// All bindings when document ready
+$(document).ready(function() {
+   // Set canvas dimensions at startup
+   resizeCanvas();
+
+   // Auto resize canvas to keep aspect ratio
+   $(window).resize(function() {
+      resizeCanvas();
+   });
+})
